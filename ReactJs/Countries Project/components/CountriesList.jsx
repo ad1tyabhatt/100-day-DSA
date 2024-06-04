87aa7080d3ddc571b8477f816724f8a34f489938
Aditya Bhatt <adityabhatt789@gmail.com>
2024-06-04 11:41:58 +0530

    added Countries Shimmer

--- ReactJs/Countries Project/components/CountriesList.jsx
+++ ReactJs/Countries Project/components/CountriesList.jsx
@@ -1,14 +1,31 @@
-import React, { useState } from 'react'
-import countriesData from '../countriesData'
+import React, { useEffect, useState } from 'react'
+// import countriesData from '../countriesData'
 import CountryCard from './CountryCard'
+import CountriesListShimmer from './CountriesListShimmer'
 
-export default function CountriesList({query}) {
+export default function CountriesList({ query }) {
+  const [countriesData, setCountriesData] = useState([])
+
+  // const [filteredData, setQuery] = useFilter(data, () => '')
+
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then((res) => res.json())
+      .then((data) => {
+        setCountriesData(data)
+      })
+  }, [])
+
+  if (!countriesData.length) {
+    return <CountriesListShimmer />
+  }
+
   return (
     <>
       <div className="countries-container">
         {countriesData
           .filter((country) =>
-            country.name.common.toLowerCase().includes(query)
+            country.name.common.toLowerCase().includes(query) || country.region.toLowerCase().includes(query)
           )
           .map((country) => {
             return (
@@ -19,6 +36,7 @@ export default function CountriesList({query}) {
                 population={country.population}
                 region={country.region}
                 capital={country.capital?.[0]}
+                data={country}
               />
             )
           })}
