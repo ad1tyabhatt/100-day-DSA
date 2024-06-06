3156a37b2e8764602c8b16fc2e5adb9c2a7531ab
Aditya Bhatt <adityabhatt789@gmail.com>
2024-06-06 10:48:48 +0530

    Created form

---
+++ ReactJs/Form Application/src/expenseData.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: crypto.randomUUID(),
+    title: "Milk",
+    category: "Grocery",
+    amount: 50,
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Shirt",
+    category: "Clothes",
+    amount: 800,
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Vegetables",
+    category: "Grocery",
+    amount: 500,
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Electricity Bill",
+    category: "Bills",
+    amount: 1150,
+  },
+];
